7abf5a0cc41632f6a5838129e4d260154a8af7a1
yenlee38 <ngocyen174308@gmail.com>
2021-01-05 22:16:08 +0700

    yen

--- QLNV_1/View/Script/Main.js
+++ QLNV_1/View/Script/Main.js
@@ -102,7 +102,7 @@ class Employees {
             self.loadEmployeesUI(response);
         }).fail(function () {
 
-                alert("fail");
+               // alert("fail");
             });
 
 
